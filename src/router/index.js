bf6e29a472d648a96c2155aecc6cc5d6e1d1c3a0
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-26 21:25:31 +1300

    05-配置-@别名路径及vscode联想提示

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "../pages/Layout";
-import Login from "../pages/Login";
+import Layout from "@/pages/Layout";
+import Login from "@/pages/Login";
 
 // 配置路由
 const router = createBrowserRouter([
