0cf021c60165d04e1cd8fd11a641d5e3cf5b4b17
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-27 14:28:42 +1300

    12-登录-登录成功后的操作

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,26 @@
 import "./index.scss";
-import { Card, Form, Input, Button } from "antd";
+import { Card, Form, Input, Button, message } from "antd";
 import logo from "@/assets/logo.png";
 import { useDispatch } from "react-redux";
 import { fetchLogin } from "@/store/modules/user";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  // 触发异步方法的钩子
   const dispatch = useDispatch();
+  // 页面跳转的钩子
+  const navigate = useNavigate();
   // 提交表单的回调
-  const onFinish = (values) => {
+  const onFinish = async (values) => {
     console.log(values);
     // 触发异步方法，提交表单数据，是useDispatch钩子
-    dispatch(fetchLogin(values));
+    await dispatch(fetchLogin(values));
+    // 登录成功之后怎么操作？
+    // 添加async/await保证执行完异步操作之后才跳转
+    // 1. 跳转到主页，使用useNavigate钩子
+    navigate("/");
+    // 2.提示用户登录成功
+    message.success("登录成功！");
   };
   return (
     <div className="login">
